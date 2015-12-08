e99bb73263bdeccb39687d4f229c808ea2d9b6ee
fawyna <fawwaz@groventure.com>
2015-12-08 08:51:46 +0800

    Update and rename firstscript.sh to backup-dir.sh

--- backup-dir.sh
+++ backup-dir.sh
@@ -2,10 +2,10 @@
 
 set -e
 
-if [$1 -ne ""] || [$2 -ne ""]
+if [$1 -ne ""]
 then
 	echo "Please enter the variables in full"
 else
-	archive_name=$2_$(date +%d\%m\%Y).tar.xz
+	archive_name=$1.tar.xz
 	tar -cvJf $archive_name $1
 fi
